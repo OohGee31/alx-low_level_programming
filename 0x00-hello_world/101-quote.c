ea0b41ec2443323324153302b987cf994ed39010
OohGee31 <youngest4vea7@gmail.com>
2023-07-13 18:51:57 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,11 @@
 /**
  * main - "A C program that prints a line"
  *
- * Return : 1 (success)
+ * Return: 1 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
