8ac7596990fe074774628d139c058f269326d67b
OohGee31 <youngest4vea7@gmail.com>
2023-07-14 17:45:51 -0700

    Capital and Small letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char n;
+	char f;
+
+	n = "a";
+	f = "A";
+	while
+		(n <= "z") {
+			putchar(n);
+			n++;
+		}
+	while
+		(f <= "Z") {
+			putchar(f);
+			f++;
+		}
+	putchar ('\n');
+	return (0);
+}
