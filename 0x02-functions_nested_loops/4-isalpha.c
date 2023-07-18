fc4c6fe92e1018dacdcba7f4252ba416cd526e8f
OohGee31 <youngest4vea7@gmail.com>
2023-07-18 12:54:52 -0700

    Is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - Checks for alphabetic character
- * @c: The character is checked 
+ * @c: The character is checked
  * Return: 1 for alphabetic character or 0 is anything else
  *
  */
