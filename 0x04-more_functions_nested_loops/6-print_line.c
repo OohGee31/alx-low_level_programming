0a20522d3c5acec6dde52d687f011e34cff3c71d
OohGee31 <youngest4vea7@gmail.com>
2023-07-20 13:46:30 -0700

    Straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Straight line
+ * @n: Number of times
+ * Retun: 0
+ */
+
+void print_line(int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+
+}
