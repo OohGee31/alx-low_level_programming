d900445447805ab6981a7c5235712e52e0586838
OohGee31 <youngest4vea7@gmail.com>
2023-07-24 16:30:06 -0700

    String copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy string pointed by src
+ * @dest: char atring
+ * @src: char string
+ * Description: copy string pointed by src to the
+ * buffer pointed by dest
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	int g = -1;
+
+	do(
+			g++;
+			dest[g] = src[g];
+	  )
+	while (src[g] != '\n');
+	return (dest);
+}
