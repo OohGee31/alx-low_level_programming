a5b3cbd2229a424050ce9c7ae8036c41dc4aa7cc
OohGee31 <youngest4vea7@gmail.com>
2023-07-25 06:34:40 -0700

    put half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,16 +5,17 @@
  * @str : array variable
  * Return: Always 0.
  */
+
 void puts_half(char *str)
 {
-        int a;
+	int a;
 
-        for (a = 0; str[a] != '\0'; a++)
-                ;
-        a = a + 1;
-        for (a = a / 2; str[a] != '\0'; a++)
-        {
-                _putchar(str[a]);
-        }
-        _putchar('\n');
-} 
+	for (a = 0; str[a] != '\0'; a++)
+		;
+	a = a + 1;
+	for (a = a / 2; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
