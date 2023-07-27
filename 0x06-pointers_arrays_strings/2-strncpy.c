ca656fc25beac6fdf2eb257fdd17eae17388c35f
OohGee31 <youngest4vea7@gmail.com>
2023-07-27 15:23:02 -0700

    String copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: destination 
+ * @src: source
+ * @n: input value
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int q;
+
+	q = 0;
+
+	while (q < n && src[q] != '\0')
+	{
+		dest[q] = src[q];
+		q++;
+	}
+	while (q < n)
+	{
+		dest[q] = '\0';
+		q++;
+	}
+	return (dest);
+}
